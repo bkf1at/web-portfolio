ff711870f7d5ce3523c00f5eb8146cb121b0b20b
BARBARA KUFFOUR <60520775+bkf1at@users.noreply.github.com>
2023-02-15 21:14:13 -0500

    First commit

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const projects = [{
+        title: "Travel App",
+        subtitle: "HTML/CSS/Js, WordPress",
+        description: "The company engages in programs that provide students, life changing moments around the world. I updated yearly programs together with the content team using Beaver builder and UnBounce page builders.",
+        image: "./worlds.gif",
+        link: "https://worldstrides.com/",
+    },
+    {
+        title: "POS System",
+        subtitle: "WordPress",
+        description: "Company specializes in solving POS system and eCommerce website challenges among businesses. Updated existing webpages with Elementor PRO and Gutenberg with animations and illustrations.",
+        image: "./merchant.gif",
+        link: "https://merchantadvisoryservices.com/",
+    },
+    {
+        title: "Food Health App",
+        subtitle: "HTML/CSS, WordPress",
+        description: "An NGO designed to promote healthy food lifestyles in aid rural communities.  I trained the client through the whole process of setting the account up and building the WordPress page.",
+        image: "./empower.gif",
+        link: "https://knowempower.org/",
+    },
+    {
+        title: "Marketing",
+        subtitle: "HTML/CSS, WordPress",
+        description: "A solutions company that enables end-users to access third party marketing strategies through advance digital tools. I developed the application from scratch, page layout designs and building content.",
+        image: "./step.gif",
+        link: "https://stepknock.com/",
+    },
+];
+
+export const testimonials = [{
+        quote: "I would highly recommend Barbara because of how passionate she is when it comes to customer service. She has an excellent ability to effectively listen and to proceed with each task at hand. During our short period voluntarily, she was very committed and focused on delivering things on time." ,
+        image: "https://i.postimg.cc/prWmHjFV/Screenshot-20230207-191425-Instagram.jpg",
+        name: "Bernison Dukuh",
+        company: "I.T Specialist USDA - ARS",
+    },
+    {
+        quote: "Barbara is an exceptional web programmer. I would recommend her anytime, any day, and anywhere. it was a great pleasure building my website. I couldn’t be happier with the final product of my website.",
+        image: "https://i.postimg.cc/yYY3fjbr/seth.jpg",
+        name: "Adinkra Seth",
+        company: "Visual Interactive Designer at US Bank",
+    },
+];
+
+export const skills = [
+    "Node",
+    "React",
+    "MySQL",
+    "Github",
+    "Bootstrap",
+    "JavaScript",
+    "HTML/CSS",
+    "AWS S3 Bucket",
+    "Adobe Photoshop",
+    "CMS - WordPress/Shopify",
+];
