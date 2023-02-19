e26025df8a6ce2cdf17260442c760602838dfc1c
BARBARA KUFFOUR <60520775+bkf1at@users.noreply.github.com>
2023-02-18 19:14:33 -0500

    Second commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,33 @@
-//import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
-//import { projects } from "../data";
 
 export default function About() {
-
-    return ( 
-        <section id = "about" className = "relative" >
-            <div className = "container px-5 py-10 mx-auto grid grid-cols-2 gap-3">
-                <div className="pl-20 pt-20">
-                    <h1 className="text-5xl text-white pb-2">Hello, I'm Barbara</h1>
-                    <h5 className="text-2xl text-white pb-2">I like building apps that make an impact in each clients' lives.</h5>
-                    <p className="pb-6">As a freelancer web developer for the past 3 years, I have engaged in remote work for startups, trained clients' on CMS platforms and worked alongside other developers to build applications for end users. I am persistent on acquiring new skills to be competitive in my field of work while keeping abreast with new technology.</p>
-                    <a href="https://zabbyweekly.com/" className ="text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">See My Blog</a>
-                    
-                </div>
-                <div class="columns-2">
-                    <a target="_blank" rel="noreferrer">
-                        <img className="object-scale-down h-25 w-25" src = "https://i.postimg.cc/Kz6PvngX/program.png" alt="coding"></img> 
-                    </a>  
-                </div> 
-            </div> 
-        </section >
-    );
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          Hello, I'm Barbara.
+            <br className="hidden lg:inline-block" />I love to build apps that make an impact.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          As a freelancer web developer for the past 3 years, I have engaged in remote work for startups, trained clients' on CMS platforms and worked alongside other developers to build applications for end users. I am persistent on acquiring new skills to be competitive in my field of work while keeping abreast with new technology.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="https://zabbyweekly.com/"
+              className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+              See My Blog
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./code.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
