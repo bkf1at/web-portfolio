1224257eddbf8eaac771f2a889c465212d3277db
BARBARA KUFFUOR <60520775+bkf1at@users.noreply.github.com>
2024-06-20 12:58:12 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,75 @@
+
+export const projects = [
+    
+    {
+        title: "Birthday Registration",
+        subtitle: "HTML/CSS/React.Js, AWS Amplify, Dynamo DB",
+        description: "React built website with AWS Amplify and DynamoDB properties to save data on the backend to keep track of RSVPs for a birthday party registration event.",
+        image: "./nileday.gif",
+        link: "https://nilebday.netlify.app/",
+    },
+    {
+        title: "ADTRAV Travel Management",
+        subtitle: "HTML/CSS/Js, Netlify",
+        description: "A travel management software and service company that enables corporate agencies and government organizations to travel easily and efficiently as possible.",
+        image: "./adtrav.gif",
+        link: "https://myadtrav.netlify.app/",
+    },
+
+    {
+        title: "AWS Amplify: Notes App",
+        subtitle: "React/Amplify/CSS/Js, WordPress",
+        description: "The app uses a 6 digit code authentication to set up an account or log in, and allows users to save and retrieve notes with description and image at anytime.",
+        image: "./reactamplify.gif",
+        link: "https://main.d3i5itcr9od7ef.amplifyapp.com/",
+    },
+    {
+        title: "WorldStrides: Travel App",
+        subtitle: "HTML/CSS/Js, WordPress",
+        description: "The company engages in programs that provide students, life changing moments around the world. I updated yearly programs together with the content team using Beaver builder and UnBounce page builders.",
+        image: "./worlds.gif",
+        link: "https://worldstrides.com/",
+    },
+    {
+        title: "Merchant Advisory Services: POS System",
+        subtitle: "WordPress & PlugIns",
+        description: "Company specializes in solving POS system and eCommerce website challenges among businesses. Updated existing webpages with Elementor PRO and Gutenberg with animations and illustrations.",
+        image: "./merchant.gif",
+        link: "https://merchantadvisoryservices.com/",
+    },
+    {
+        title: "Knowledge & Empowerment: Food Health App",
+        subtitle: "HTML/CSS, WordPress",
+        description: "An NGO designed to promote healthy food lifestyles in aid rural communities.  I trained the client through the whole process of setting the account up and building the WordPress page.",
+        image: "./empower.gif",
+        link: "https://knowempower.org/",
+    },
+    
+];
+
+export const testimonials = [{
+        quote: "I would highly recommend Barbara because of how passionate she is when it comes to customer service. She has an excellent ability to effectively listen and to proceed with each task at hand. During our short period voluntarily, she was very committed and focused on delivering things on time." ,
+        image: "https://i.postimg.cc/prWmHjFV/Screenshot-20230207-191425-Instagram.jpg",
+        name: "Bernison Dukuh",
+        company: "I.T Specialist USDA - ARS",
+    },
+    {
+        quote: "Barbara has worked on several high-profile projects, and in each case, she delivered outstanding results. Her ability to manage multiple projects simultaneously while still maintaining the highest level of quality is awe-inspiring. Overall, Barbara is an exceptional web designer who would be a great asset to any organization. ",
+        image: "https://i.postimg.cc/yYY3fjbr/seth.jpg",
+        name: "Adinkra Seth",
+        company: "Visual Interactive Designer at US Bank",
+    },
+];
+
+export const skills = [
+    "Node",
+    "React",
+    "MySQL",
+    "Github",
+    "Bootstrap",
+    "JavaScript",
+    "HTML/CSS",
+    "AWS S3 Bucket",
+    "Adobe PhotoShop",
+    "CMS - WordPress/Shopify",
+];
